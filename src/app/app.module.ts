55318d75ed84f01633a0b63ac7bda47ecb87eea4
ShilpaLalwani <shilpal.2507@gmail.com>
2020-06-29 14:00:07 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { EventCardComponent } from './event-card/event-card.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, FontAwesomeModule, CoreModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, EventCardComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faArrowRight);
+  }
+}
